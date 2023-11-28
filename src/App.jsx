9ae5b65720a989b3c76368f04b0b7d567c412c55
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-11-28 12:33:30 +0500

    delete item

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,49 @@ import './App.css'
 
 function App() {
   const [name, setName] = useState(0)
-
-  const clik = () => {
-      setName(name+1)
+  const [data , setData] = useState([
+    {
+      id:1,
+      nameData:"birinchi",
+      email:"hjkjhkj@.com"
+    },
+    {
+      id:2,
+      nameData:"ikkinch",
+      email:"hjkjhkj@.com"
+    },
+    {
+      id:3,
+      nameData:"uchinchi",
+      email:"hjkjhkj@.com"
+    },
+  ])
+ 
+   const del = ( id) =>{
+    const filterDel = data.filter((item2)=> {
+      return item2.id !== id
+    })
+    setData(filterDel)
   }
   return(
     <>
-      <h1>{name}</h1>
-      <button onClick={clik}> tugma</button>
+      
+      {/* <button onClick={list}>data</button> */}
+      {data.map((item) => {
+        return(
+          <ul key={item.id}>
+            <li>{item.nameData}</li> 
+            <li>{item.id}</li> 
+            <li>{item.email}</li> 
+            <button onClick={ ()=> del(item.id)} >del</button> 
+          </ul>
+        )
+      })}
     </>
   )
 }
 
 export default App
+
+
+
